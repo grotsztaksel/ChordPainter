bac15b95fc1f9a654f960ecb2a4fb2f29f65b1a5
grotsztaksel <grotsztaksel@o2.pl>
2021-10-16 19:40:52 +0200

    Use QApplication to avoid crash on QPixmap initialization

--- main.py
+++ main.py
@@ -8,13 +8,36 @@ Created on 12.10.2021 21:54
 __authors__ = ['Piotr Gradkowski <grotsztaksel@o2.pl>']
 __date__ = '2021-10-12'
 
-def main(name):
+import os
+import sys
+
+from PyQt5.QtCore import Qt, QSize
+from PyQt5.QtWidgets import QApplication
+
+from chord import Chord
+from chord_painter import ChordPainter
+
+
+
+def main():
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
+    pass
+
+
 if __name__ == '__main__':
-    main('PyCharm')
+    app = QApplication(sys.argv)
+    # Using a QApplicaion appears to mitigate the crasch that would otherwise occur opun constructing a QPixmap.
+    #
+    size = QSize(480, 640)
+    crd = Chord("c", ((2, 1), 0, (2, 2), (2, 3)))
+    painter = ChordPainter(crd, size)
+    painter.drawStrings()
+    painter.drawFrets()
+    painter.p.end()
+    px = painter.pixmap
+    px.save("example.png", "PNG")
+    sys.exit(app.exit())
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
