5935823aa446aaa4e56a51c89d35f21d9f607d80
grotsztaksel <grotsztaksel@o2.pl>
2021-10-16 18:53:45 +0200

    Implement specialized methods for drawing of diagram elements

--- chord_painter.py
+++ chord_painter.py
@@ -9,84 +9,194 @@ __all__ = ['ChordPainter']
 __date__ = '2021-10-12'
 __authors__ = ["Piotr Gradkowski <grotsztaksel@o2.pl>"]
 
-import os
+from PyQt5.QtCore import Qt, QSize, QPoint, QRect
+from PyQt5.QtGui import QColor, QPainter, QPixmap, QBrush
 
-from PyQt5.QtCore import Qt, QSize
-from PyQt5.QtGui import QColor, QPainter, QPixmap
+from chord import Chord
 
 
 class ChordPainter(object):
     """
-    The class actually responsible for painting the chord schemes and saving image files from them
+    The class actually responsible for painting the chord scheme and saving image files from them
     """
 
-    def __init__(self):
-        self.string_offset = 40  # pixels
-        self.string_thickness = 2
-        self.bar_zero_width = 4
-        self.fret_width = 120  # pixels
-        self.finger_size = 16  # pixels
-        self.dots_on_frets = False  # If True, the painter will draw dots on 3rd, 5, 7 etc frets
+    def __init__(self, chord=None, size=None):
 
-        self.size = QSize(640, 480)
-        self.colors = {"string": QColor(Qt.black),
-                       "fret": QColor(Qt.black),
-                       "finger": QColor(Qt.black),
-                       "fret_dot": QColor(Qt.gray)}
+        self.setChord(chord)
+        self.setSize(size)
 
-        self.dir = os.getcwd()
-        self.orientation = Qt.Vertical
+        self.string_thickness = 2  # width of the string lines (pixels)
 
-    def setDir(self, dir):
+        self.nfrets = 5  # number of frets visible
+
+        self.openStringMarkSize = 20  # pixels
+        self.muteStringMarkSize = 20  # pixels
+
+        self.p = QPainter()
+        self.firstFretVisible = 0
+
+        self.pos_string = []
+        self.pos_fret = []
+
+    def setChord(self, chord):
         """
-        Set directory where the files should be saved
+        Set the chord scheme
         """
-        if os.path.isdir(dir):
-            self.dir = dir
+        assert isinstance(chord, Chord)
+        self.chord = chord
 
-    def drawStrings(self, scheme, pixmap):
-        pass
-
-    def drawFrets(self, scheme, pixmap):
-        pass
-
-    def drawChord(self, scheme) -> QPixmap:
+    def setSize(self, size: QSize):
         """
-        Set up a QPainter object and use scheme to draw a chord
-        :param scheme: a tuple of tuples of numbers encoding the chord
-               for example:
-               ((0), (1,1), (2,3))
-               will paint a three-stringed fretboard (three elements in the tuple); the first string is not pressed,
-               the second is pressed on the 1st fret with 1st finger; the third is pressed on 2nd fret with 3rd finger
-
-               If each "subtuple" has only one element, the dots on the chord diagram will not have finger numbers
-
-        :return: a QPainter object with the chord scheme
+        Set the size of the output image
         """
-        px = QPixmap(self.size)
-        w = self.size.width()
-        h = self.size.height()
-        p = QPainter(px)
+        if size is None:
+            return
+        self.pixmap = QPixmap(size)
 
-        margin = self.finger_size / 2
-        pen = p.pen()
-        pen.setColor(self.colors["fret"])
-        pen.setWidth(self.bar_zero_width)
-        p.drawLine(margin, margin, w - margin, margin)
+    def size(self):
+        return self.pixmap.size()
 
-        pen.setColor(self.colors["string"])
-        pen.setWidth(self.string_thickness)
+    def drawChord(self):
+        for s, i in enumerate(self.chord.scheme):
+            if not isinstance(s, int):
+                s = s[0]
+            if s < 0:
+                self.drawMuteString(i)
+            elif s == 0:
+                self.drawOpenString(i)
+            else:
+                self.drawFinger(i)
 
-        n = len(scheme) - 1
-        if n == 0:
-            # Draw only one string
-            string_positions = [w / 2]
+    def drawStrings(self):
+        string_thickness = 2
+        string_color = Qt.black
+
+        w = self.pixmap.width()
+        h = self.pixmap.height()
+
+        n = len(self.chord)
+        offset = w / n
+        margin = int(0.5 * offset)
+
+        self.pos_string = [margin + i * offset for i in range(n)]
+
+        pen = self.p.pen()
+        pen.setColor(string_color)
+        pen.setWidth(string_thickness)
+        self.p.setPen(pen)
+
+        for sp in self.pos_string:
+            self.p.drawLine(sp, margin, sp, h - margin)
+
+    def drawFrets(self):
+        color = Qt.black
+        bar_zero_width = 4  # width of the 0-th fret (pixels)
+        fret_line_width = 2
+        next_fret_fragment = 0.1  # draw the strings little bit longer than to the last fret, so thay slightly extend
+        #                           (by a fraction of the fret width)
+
+        h = self.pixmap.height()
+        fret_first = int(max(self.muteStringMarkSize, self.openStringMarkSize) * 1.1)
+        fingerboard_length = h - fret_first
+        self.fret_width = int((fingerboard_length) / (self.nfrets + next_fret_fragment))
+        self.pos_fret = [fret_first + i * self.fret_width for i in range(self.nfrets + 1)]
+
+        # Draw all frets
+        pen = self.p.pen()
+        pen.setWidth(fret_line_width)
+        pen.setColor(color)
+        self.p.setPen(pen)
+        for f in self.pos_fret:
+            self.p.drawLine(self.pos_string[0], f, self.pos_string[-1], f)
+
+        # If the zeroth fret is visible on the diagram (i.e the diagram does not start from, for example, fret III,
+        # draw the bar
+        frets_used = []
+        for string in self.chord.scheme:
+            if isinstance(string, int):
+                s = string
+            else:
+                s = string[0]
+            frets_used.append(s)
+        fret_min = min(frets_used)
+        fret_max = max(frets_used)
+        if fret_max - fret_min > self.nfrets:
+            raise ValueError(
+                "Chord {} diagram cannot be shown on range of {} frets".format(self.chord.name, self.nfrets))
+
+        if fret_max <= self.nfrets:
+            self.firstFretVisible = 0
+            # Draw the 0th fret
+            pen = self.p.pen()
+            pen.setWidth(bar_zero_width)
+            pen.setColor(color)
+            self.p.setPen(pen)
+            self.p.drawLine(self.pos_fret[0], self.pos_string[0], self.pos_fret[0], self.pos_string[-1])
         else:
-            string_positions = [margin + i * (w - self.finger_size) / n for i in range(n)] + [w - margin]
+            self.firstFretVisible = fret_min - 1
 
-        for sp in string_positions:
-            p.drawLine(sp, margin, sp, h - margin)
+    def drawFinger(self, istring):
+        margin = 0
+        color = Qt.black
+        offsetFromFret = 0.2  # Of the fret width
+        labelFinger = False
+        labelColor = Qt.white
+        pen = self.p.pen()
+        brush = QBrush()
+        brush.setColor(color)
 
-        p.end()
+        pen.setColor(color)
+        pen.setBrush(brush)
+        self.p.setPen()
 
-        return px
+        rect = self._getMarkerRect(istring, margin)
+        fret = self.chord.scheme(istring)
+        if isinstance(fret, int):
+            finger = None
+        else:
+            finger = fret[1]
+            fret = fret[0]
+
+        rect.moveBottom(self.pos_fret[fret - self.firstFretVisible] - int(self.fret_width * offsetFromFret))
+
+        self.p.drawEllipse(rect)
+
+        if finger not in list(range(5)) or not labelFinger:
+            return
+
+        pen = self.p.pen()
+        pen.setColor(labelColor)
+        self.p.setPen(pen)
+
+        self.p.drawText(rect, Qt.AlignCenter, str(finger), rect)
+
+    def drawOpenString(self, istring):
+        lineWidth = 2
+        margin = 2
+        pen = self.p.pen()
+        pen.setBrush(Qt.NoBrush)
+        pen.setWidth(lineWidth)
+        self.p.setPen(pen)
+
+        self.p.drawEllipse(self._getMarkerRect(istring, margin))
+
+    def drawMuteString(self, istring):
+        lineWidth = 2
+        margin = 2
+        pen = self.p.pen()
+        pen.setBrush(Qt.NoBrush)
+        pen.setWidth(lineWidth)
+        self.p.setPen(pen)
+
+        rect = self._getMarkerRect(istring, margin)
+
+        self.p.drawLine(rect.topLeft(), rect.bottomRight())
+        self.p.drawLine(rect.topRight(), rect.bottomLeft())
+
+    def _getMarkerRect(self, istring, margin):
+        topLeft = QPoint(self.pos_string[istring] - self.openStringMarkSize / 2 + margin,
+                         margin)
+        bottomRight = QPoint(self.pos_string[istring] + self.openStringMarkSize / 2 - margin,
+                             self.openStringMarkSize - margin)
+        rect = QRect(topLeft, bottomRight)
+        return rect
