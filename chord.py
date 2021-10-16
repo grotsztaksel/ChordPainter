72463cfd26f21439b8ac6a3446d36254f91589f5
grotsztaksel <grotsztaksel@o2.pl>
2021-10-16 19:39:57 +0200

    Propery define length of a Chord

--- chord.py
+++ chord.py
@@ -16,8 +16,14 @@ Chord = namedtuple("Chord", ["name", "scheme"])
 Chord.__doc__ = """
                 A tuple representing a chord. 
                 An instrument can have multiple chords with the same name as long as their diagram schemes are different
+
                 """
-Chord.__len__ = len(Chord.scheme)
+
+
+class Chord(Chord):
+    def __len__(self):
+        return len(self.scheme)
+
 
 CHORD_PATTERN = re.compile(
     r"([A-Ha-h]|(A-H)(maj|m)?)(\+|0|6|6/9|7|7b5|7sus4|9|sus2|sus4|add9)"  # This can probably get better
