79a95d820faa82fcff4e6994135c31d1108645c7
grotsztaksel <grotsztaksel@o2.pl>
2021-10-17 15:33:59 +0200

    Show the current chord image in a GUI dialog

--- main.py
+++ main.py
@@ -8,36 +8,27 @@ Created on 12.10.2021 21:54
 __authors__ = ['Piotr Gradkowski <grotsztaksel@o2.pl>']
 __date__ = '2021-10-12'
 
-import os
 import sys
 
-from PyQt5.QtCore import Qt, QSize
+from PyQt5.QtCore import QSize
 from PyQt5.QtWidgets import QApplication
 
 from chord import Chord
 from chord_painter import ChordPainter
-
-
-
-def main():
-    # Use a breakpoint in the code line below to debug your script.
-
-
-    pass
-
+from main_window import MainWindow
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    # Using a QApplicaion appears to mitigate the crasch that would otherwise occur opun constructing a QPixmap.
+    # Using a QApplication appears to mitigate the crasch that would otherwise occur upon constructing a QPixmap.
     #
-    size = QSize(480, 640)
+    size = QSize(160, 240)
     crd = Chord("c", ((2, 1), 0, (2, 2), (2, 3)))
     painter = ChordPainter(crd, size)
-    painter.drawStrings()
-    painter.drawFrets()
+    painter.strings.draw()
+    painter.frets.draw()
     painter.p.end()
     px = painter.pixmap
-    px.save("example.png", "PNG")
-    sys.exit(app.exit())
+    mw = MainWindow(None, px)
+    mw.exec()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    sys.exit(app.exit())
---
+++ main_window.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 17.10.2021 15:27 10
+ 
+@author: Piotr Gradkowski <grotsztaksel@o2.pl>
+"""
+
+__all__ = ['MainWindow']
+__date__ = '2021-10-17'
+__authors__ = ["Piotr Gradkowski <grotsztaksel@o2.pl>"]
+
+from PyQt5.QtCore import Qt, QSize
+from PyQt5.QtGui import QPalette, QBrush
+from PyQt5.QtWidgets import QDialog
+
+
+class MainWindow(QDialog):
+    def __init__(self, parent, pixmap):
+        super().__init__(parent, Qt.Dialog)
+        self.setFixedSize(pixmap.size() + QSize(1, 1))
+
+        palette = self.palette()
+        palette.setBrush(QPalette.Background, QBrush(pixmap))
+
+        self.setPalette(palette)
+
+    def keyPressEvent(self, event) -> None:
+        """
+        Press just any key to close the window
+        """
+        self.close()
