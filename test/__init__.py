74e6d6d5936a45f937c4386c078ec34adda16151
grotsztaksel <grotsztaksel@o2.pl>
2021-12-04 22:34:20 +0100

    Move tests to separate dir

---
+++ test/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 04.12.2021 22:31 12
+ 
+@author: Piotr Gradkowski <grotsztaksel@o2.pl>
+"""
+
+
+__date__ = '2021-12-04'
+__authors__ = ["Piotr Gradkowski <grotsztaksel@o2.pl>"]
