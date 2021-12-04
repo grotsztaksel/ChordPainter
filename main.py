c4c47d0897b2517ed6ab26cd430a611438decded
grotsztaksel <grotsztaksel@o2.pl>
2021-12-05 00:16:56 +0100

    Show fretboard in a GUI window

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ from PyQt5.QtWidgets import QApplication
 from Instruments.banjo import Banjo_5string as Banjo
 from chord_painter import ChordPainter
 from file_register import FileRegister
+from fretboard_painter import FretboardPainter
 from main_window import MainWindow
 
 if __name__ == '__main__':
@@ -33,25 +34,28 @@ if __name__ == '__main__':
     if not os.path.isdir(targetDir):
         os.makedirs(targetDir)
 
-    for chord in banjo.chords:
-        painter = ChordPainter(chord, size, banjo)
-        painter.drawEmpty()
-        painter.drawChord()
-        painter.p.end()
-        px = painter.pixmap
+    painter = FretboardPainter(size, banjo)
+    px = painter.pixmap
+    mw = MainWindow(None, px)
+    mw.setWindowTitle("Banjo")
+    mw.exec()
+    # for chord in banjo.chords:
+    #     painter = ChordPainter(chord, size, banjo)
+    #     painter.drawEmpty()
+    #     painter.drawChord()
+    #     painter.p.end()
+    #     px = painter.pixmap
+    #
+    #     filesSaved = []
+    #     filenum = 0
+    #     ext = ".png"
+    #     fileName = chord.prefix + chord.toString + chord.suffix + ext
+    #
+    #     pngName = filereg.getUniqueName(os.path.join(targetDir, fileName))
+    #     px.save(pngName)
+    #     print("Saved ", pngName)
+    #     filereg.register(pngName)
 
-        filesSaved = []
-        filenum = 0
-        ext = ".png"
-        fileName = chord.prefix + chord.toString + chord.suffix + ext
 
-        pngName = filereg.getUniqueName(os.path.join(targetDir, fileName))
-        px.save(pngName)
-        print("Saved ", pngName)
-        filereg.register(pngName)
-
-        mw = MainWindow(None, px)
-        mw.setWindowTitle(chord.name)
-        # mw.exec()
 
     sys.exit(app.exit())
