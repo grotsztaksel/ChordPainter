a6cc9c4f91bd404f62f41b7631ddb392fa92897a
grotsztaksel <grotsztaksel@o2.pl>
2021-12-06 00:31:23 +0100

    Implement save image mechanism (not working)

--- GUI/mainwindow.py
+++ GUI/mainwindow.py
@@ -12,7 +12,9 @@ __authors__ = ["Piotr Gradkowski <grotsztaksel@o2.pl>"]
 import os
 
 from PyQt5 import uic
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, pyqtSlot, QRect
+from PyQt5.QtGui import QImage, QPainter
+from PyQt5.QtWidgets import QFileDialog
 
 from GUI.fretboard_model import FretboardDelegate
 
@@ -29,6 +31,8 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         hmin = self.tableView.verticalHeader().defaultSectionSize()
         self.tableView.verticalHeader().setDefaultSectionSize(max(hmin, 45))
 
+        self.saveButton.clicked.connect(self.onSaveButtonClicked)
+
     def adjustSizes(self):
         model = self.tableView.model()
         w = 0
@@ -36,3 +40,19 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         for i in range(ncol):
             w = max(w, self.tableView.sizeHintForColumn(i))
         self.tableView.horizontalHeader().setDefaultSectionSize(w)
+
+    @pyqtSlot()
+    def onSaveButtonClicked(self):
+        filename = QFileDialog.getSaveFileName(self, "Save fretboard", filter="*.png;; *.bmp;; *.jpg")
+        if not filename:
+            return
+        self.saveFretboard(filename[0])
+
+    def saveFretboard(self, fileName):
+
+        img = QImage(self.tableView.size(), QImage.Format_RGB32)
+        p = QPainter(img)
+        self.tableView.render(p)
+
+        if img.save(fileName):
+            print ("Saved ", fileName)
