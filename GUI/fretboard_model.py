eab3ad818d0b36268825fa299752c30acba8bb31
grotsztaksel <grotsztaksel@o2.pl>
2021-12-05 23:17:41 +0100

    Add a custom delegate and option to edit tuning

--- GUI/fretboard_model.py
+++ GUI/fretboard_model.py
@@ -12,9 +12,11 @@ __authors__ = ["Piotr Gradkowski <grotsztaksel@o2.pl>"]
 import typing
 
 from PyQt5.QtCore import Qt, QAbstractItemModel, QModelIndex
+from PyQt5.QtWidgets import QStyledItemDelegate, QWidget, QSpinBox
 
 from Instruments.instrument import Instrument
 from chord_inventor import ChordInventor
+from music_theory import NOTES
 
 
 class FretboardModel(QAbstractItemModel):
@@ -26,7 +28,7 @@ class FretboardModel(QAbstractItemModel):
         super().__init__(parent)
         self.instrument = instrument
         self.chordInventor = ChordInventor(self.instrument)
-        self.editable = False
+        self.editable = True
 
     def hasIndex(self, row: int, column: int, parent: QModelIndex = ...) -> bool:
         if parent.isValid():
@@ -72,3 +74,46 @@ class FretboardModel(QAbstractItemModel):
             return Qt.AlignCenter
         elif role == Qt.DisplayRole or role == Qt.EditRole:
             return self.instrument.getNote(len(self.instrument.strings) - index.column() - 1, index.row())
+
+    def setData(self, index: QModelIndex, value: typing.Any, role: int = Qt.EditRole) -> bool:
+        if role != Qt.EditRole:
+            return False
+        if not self.editable:
+            return False
+        string = len(self.instrument.strings) - index.column() - 1
+        self.instrument.strings[string] = value
+        return True
+
+
+class RootNoteSpinBox(QSpinBox):
+    """Special spinbox to edit notes"""
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setMinimum(-1)
+        self.setMaximum(len(NOTES) + 1)
+        self.setValue(0)
+
+    def textFromValue(self, v: int) -> str:
+        return NOTES[v % len(NOTES)]
+
+    def valueFromText(self, text: str) -> int:
+        return NOTES.index(text)
+
+    def stepBy(self, step):
+        if self.value() == 0 and step == -1:
+            self.setValue(self.maximum())
+        elif self.value() == self.maximum() - 1 and step == 1:
+            self.setValue(0)
+        super(RootNoteSpinBox, self).stepBy(step)
+
+
+class FretboardDelegate(QStyledItemDelegate):
+    def createEditor(self, parent: QWidget, option: 'QStyleOptionViewItem', index: QModelIndex) -> QWidget:
+        return RootNoteSpinBox()
+
+    def setEditorData(self, editor: QWidget, index: QModelIndex) -> None:
+        editor.setValue(NOTES.index(index.data(Qt.EditRole)))
+
+    def setModelData(self, editor: QWidget, model: QAbstractItemModel, index: QModelIndex) -> None:
+        model.setData(index, editor.textFromValue(editor.value()), Qt.EditRole)
