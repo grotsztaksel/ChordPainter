568d636cc569e208ee6499190798b5558ad451dd
grotsztaksel <grotsztaksel@o2.pl>
2021-12-14 18:56:19 +0100

    Add functionality in ChordSelector widget

--- GUI/chord_selector.py
+++ GUI/chord_selector.py
@@ -9,7 +9,8 @@ __date__ = '2021-12-12'
 import os
 
 from PyQt5 import uic
-from PyQt5.QtWidgets import QButtonGroup, QRadioButton, QGridLayout
+from PyQt5.QtCore import pyqtSignal, pyqtSlot
+from PyQt5.QtWidgets import QButtonGroup, QRadioButton, QGridLayout, QAbstractButton
 
 from music_theory import NOTES, ChordInterval
 
@@ -17,6 +18,8 @@ Ui_ChordSelector, QWidget = uic.loadUiType(os.path.join(os.path.dirname(__file__
 
 
 class ChordSelector(QWidget, Ui_ChordSelector):
+    chordSelected = pyqtSignal(str, str)
+
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
@@ -24,11 +27,16 @@ class ChordSelector(QWidget, Ui_ChordSelector):
         self.grid = QGridLayout(self)
         self.verticalLayout.insertLayout(1, self.grid)
 
+        self.clearButton.clicked.connect(self.clear)
         self._setupRadioButtons()
+
         self._setupComboBox()
 
     def _setupComboBox(self):
-        self.chordTypeComboBox.addItems([c.name for c in ChordInterval.getAllChordTypes__()])
+        chordTypes = [c.name for c in ChordInterval.getAllChordTypes__()]
+        self.chordTypeComboBox.addItems(chordTypes)
+        self.chordTypeComboBox.setCurrentIndex(chordTypes.index("major"))
+        self.chordTypeComboBox.currentTextChanged.connect(self.onChordTypeSelected)
 
     def _setupRadioButtons(self):
         col = 0
@@ -45,3 +53,30 @@ class ChordSelector(QWidget, Ui_ChordSelector):
             self.grid.addWidget(btn, row, col)
             self.chordRootButtons.addButton(btn, i)
             row += 1
+        self.chordRootButtons.buttonToggled.connect(self.onButtonToggled)
+
+    @pyqtSlot(QAbstractButton, bool)
+    def onButtonToggled(self, button, checked):
+        if not checked:
+            return
+
+        root = button.text()
+        chordType = self.chordTypeComboBox.currentText()
+        self.chordSelected.emit(root, chordType)
+
+    @pyqtSlot(str)
+    def onChordTypeSelected(self, chordType):
+        btn = self.chordRootButtons.checkedButton()
+        if btn is None:
+            return
+
+        self.chordSelected.emit(btn.text(), chordType)
+
+    @pyqtSlot()
+    def clear(self):
+        self.chordRootButtons.blockSignals(True)
+        for button in self.chordRootButtons.buttons():
+            button.setChecked(False)
+        self.chordRootButtons.blockSignals(False)
+
+        self.chordSelected.emit("", "")
