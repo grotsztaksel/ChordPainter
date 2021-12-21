c1c6fa0311be719420c8cc567bc98b618f1ea13a
grotsztaksel <grotsztaksel@o2.pl>
2021-12-21 11:36:57 +0100

    Reorganize GUI

--- GUI/mainwindow.py
+++ GUI/mainwindow.py
@@ -11,16 +11,16 @@ __authors__ = ["Piotr Gradkowski <grotsztaksel@o2.pl>"]
 
 import json
 import os
+from copy import copy
 
 from PyQt5 import uic
-from PyQt5.QtCore import Qt, pyqtSlot, QRect, QMargins, QPoint, QObject, QEvent
+from PyQt5.QtCore import Qt, pyqtSlot, QRect, QMargins, QPoint, QObject, QEvent, QModelIndex
 from PyQt5.QtGui import QImage, QPainter, QRegion
-from PyQt5.QtWidgets import QFileDialog, QStyle, QTableView, QToolButton
+from PyQt5.QtWidgets import QFileDialog, QStyle, QTableView, QToolButton, QLineEdit
 
 import Instruments
 from GUI.define_instrument_dialog import DefineInstrumentDialog
 from GUI.fretboard_model import FretboardDelegate, FretboardModel
-from GUI.note_line_edit import NoteLineEdit
 from Instruments.instrument import Instrument
 
 Ui_MainWindow, QMainWindow = uic.loadUiType(os.path.join(os.path.dirname(__file__), "mainwindow.ui"))
@@ -30,8 +30,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None, flags=Qt.WindowFlags()):
         super().__init__(parent, flags)
         self.setupUi(self)
-
-        self.newInstrumentButton.clicked.connect(self.onNewInstrumentClicked)
+        self.model = None
 
         self.saveFretboardButton = QToolButton(self)
         self.saveFretboardButton.setIcon(self.style().standardIcon(QStyle.SP_DialogSaveButton))
@@ -57,13 +56,22 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self._readData()
 
         self.instrumentComboBox.addItems(self.getInstrumentList())
-        self.instrumentComboBox.currentIndexChanged.connect(self.onInstrumentSelected)
+        self.instrumentComboBox.insertSeparator(self.instrumentComboBox.count())
+        self.instrumentComboBox.addItem("new...")
+        self.instrumentComboBox.activated.connect(self.onInstrumentSelected)
+        self.tuningComboBox.currentIndexChanged.connect(self.onTuningSelected)
+
+        self.saveTuningButton.setIcon(self.style().standardIcon(QStyle.SP_DialogSaveButton))
+        self.saveTuningButton.clicked.connect(self.onSaveTuningClicked)
+        self.saveTuningButton.setEnabled(False)
+
+        self.tuningNameEditor = QLineEdit(self)
+        self.tuningNameEditor.hide()
+        self.tuningNameEditor.returnPressed.connect(self.saveTuning)
+        self.tuningNameEditor.editingFinished.connect(lambda: self.tuningNameEditor.hide())
 
         self.onInstrumentSelected(self.instrumentComboBox.currentIndex())
         self.adjustSizes()
-
-        notesEditor = NoteLineEdit()
-        self.tuningComboBox.setLineEdit(notesEditor)
 
     def _readData(self):
         jfile = os.path.join(os.path.dirname(Instruments.instrument.__file__), "instruments.json")
@@ -72,11 +80,13 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.jdata = json.loads(data)
 
     def setModel(self, model):
-        if self.fretboardView.model() is not None:
+        if self.model is not None:
             self.chordSelector.disconnect()
-            self.fretboardView.model().deleteLater()
+            self.model.deleteLater()
+        self.model = model
         self.fretboardView.setModel(model)
         self.chordSelector.chordSelected.connect(model.setCurrentChord)
+        self.model.dataChanged.connect(self.checkTuning)
 
         # Show the same chord on the new instrument
         self.chordSelector.emitChord()
@@ -92,16 +102,87 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
     @pyqtSlot(int)
     def onInstrumentSelected(self, i):
+        if i >= len(self.jdata["instrument"]):
+            return self.onNewInstrumentClicked()
+
         newInstrument = Instrument.fromData(self.jdata["instrument"][i])
         newModel = FretboardModel(self, newInstrument)
         self.setModel(newModel)
+        self.tuningComboBox.clear()
+        for t in self.model.instrument.tuning:
+            notes = ", ".join(t[1])
+            name = t[0]
+            if name is None:
+                self.tuningComboBox.addItem(notes)
+            else:
+                self.tuningComboBox.addItem(f"{name} ({notes})")
+
+    @pyqtSlot(int)
+    def onTuningSelected(self, i):
+        tuning = self.model.instrument.tuning[i]
+
+        row = 0
+        for string, note in enumerate(reversed(tuning[1])):
+            index = self.model.index(row, string)
+            self.model.setData(index, note, Qt.EditRole)
+
+        index_from = self.model.index(row, 0)
+        index_to = self.model.index(row, len(tuning[1]) - 1)
+        self.model.dataChanged.emit(index_from, index_to)
+
+    @pyqtSlot()
+    def onSaveTuningClicked(self):
+        """
+        Show a line editor allowing to enter the new tuning name. 
+        If a name is not selected, the tuning is saved without name
+        """
+        name = ", ".join(self.model.instrument.strings)
+        self.tuningNameEditor.setGeometry(
+            QRect.united(self.tuningComboBox.geometry(), self.saveTuningButton.geometry()))
+        # self.tuningNameEditor.move(self.tuningComboBox.mapTo(self, self.tuningComboBox.geometry().topLeft()))
+        self.tuningNameEditor.show()
+        self.tuningNameEditor.setText(name)
+        self.tuningNameEditor.selectAll()
+        self.tuningNameEditor.setFocus(Qt.OtherFocusReason)
+
+    @pyqtSlot()
+    def saveTuning(self):
+        name = self.tuningNameEditor.text()
+        s = copy(self.model.instrument.strings)
+        if name == ", ".join(s):
+            name = None
+
+        self.model.instrument.tuning.append((name, s))
+        self.tuningComboBox.addItem(f"{name} ({', '.join(s)})")
+        self.tuningNameEditor.hide()
+
+    @pyqtSlot(QModelIndex, QModelIndex)
+    def checkTuning(self, tlIndex=QModelIndex(), brIndex=QModelIndex()):
+        """
+        Checks if the currently (manually) set tuning can be found in the list of tunings.
+        If yes, select it from the combo box. If not, offer the option to save the new custom tuning
+        """
+        if tlIndex.row() != 0 and brIndex.row() != 0:
+            return
+        tuningIsKnown = False
+        for i, t in enumerate(self.model.instrument.tuning):
+            if t[1] == self.model.instrument.strings:
+                tuningIsKnown = True
+                break
+
+        self.saveTuningButton.setEnabled(not tuningIsKnown)
+
+        if tuningIsKnown:
+            self.tuningComboBox.blockSignals(True)
+            self.tuningComboBox.setCurrentIndex(i)
+            self.tuningComboBox.blockSignals(False)
 
     @pyqtSlot(str)
     def addInstrument(self, instr):
         """Add a new instrument definition to the database"""
         newInstr = json.loads(instr)
         self.jdata["instrument"].append(newInstr)
-        self.instrumentComboBox.addItem(newInstr["name"])
+        self.instrumentComboBox.insertItem(self.instrumentComboBox.count() - 2, newInstr["name"])
         self.instrumentComboBox.setCurrentIndex(self.instrumentComboBox.count() - 1)
 
     @pyqtSlot()
@@ -112,7 +193,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         dialog.exec()
 
     def adjustSizes(self):
-        model = self.fretboardView.model()
+        model = self.model
         w = 0
         ncol = model.columnCount()
         for i in range(ncol):
@@ -164,7 +245,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         encompass the entire fretboard. Otherwise would not be able to render the parts that are clipped from
         the scroll area
         """
-        model = self.fretboardView.model()
+        model = self.model
         tmpView = QTableView()
         tmpView.verticalHeader().setDefaultSectionSize(self.fretboardView.verticalHeader().defaultSectionSize())
         tmpView.horizontalHeader().setDefaultSectionSize(self.fretboardView.horizontalHeader().defaultSectionSize())
